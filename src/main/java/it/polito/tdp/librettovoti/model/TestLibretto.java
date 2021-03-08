4a6d466eac43690a76ea104205cf9391198d8efe
utente <utente@DESKTOP-5KOJBOQ>
2021-03-08 10:58:38 +0100

    Prima implementazione

---
+++ src/main/java/it/polito/tdp/librettovoti/model/TestLibretto.java
@@ -0,0 +1,15 @@
+package it.polito.tdp.librettovoti.model;
+
+import java.time.*;
+
+public class TestLibretto {
+	
+	public static void main (String[] args) {
+	    Libretto libretto=new Libretto();
+	    Voto voto1=new Voto ("Analisi I", 30, LocalDate.of(2019, 2, 15));
+	    libretto.setVoti(voto1);
+	    libretto.setVoti(new Voto ("Fisica I", 28, LocalDate.of(2019,  7, 15)));
+	    libretto.setVoti(new Voto ("Informatica", 24, LocalDate.of(2019, 9, 3)));
+	    System.out.println(libretto);
+	}
+}
